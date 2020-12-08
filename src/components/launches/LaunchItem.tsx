e695c088bf173aa5cfb3a8388e5b96f1bd1c7e5c
Sam <samshum90@gmail.com>
2020-12-08 23:13:36 +0000

    completed list item styling

--- src/components/launches/LaunchItem.tsx
+++ src/components/launches/LaunchItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Launch } from "../../types/Launch";
 
 interface Props {
@@ -6,13 +7,18 @@ interface Props {
 }
 
 const LaunchItem: React.FC<Props> = ({ launch }: Props) => {
+  const moment = require("moment");
   return (
     <li className="launch__list-item">
       <h2 className="launch__list-item-number">#{launch.flight_number}</h2>
-      <h2 className="launch__list-item-name">{launch.mission_name}</h2>
+      <h2 className="launch__list-item-flight">{launch.mission_name}</h2>
       <div className="launch__list-item-end">
-        <span>{launch.launch_date_utc}</span>
-        <span>{launch.rocket.rocket_name}</span>
+        <span className="launch__list-item-date">
+          {moment(`${launch.launch_date_utc}`).format("Do MMM YYYY", true)}
+        </span>
+        <span className="launch__list-item-rocket">
+          {launch.rocket.rocket_name}
+        </span>
       </div>
     </li>
   );
