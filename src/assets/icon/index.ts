a54bb12ef116755f8c169da964e36ed51fd30561
Sam <samshum90@gmail.com>
2020-12-08 15:52:28 +0000

    finished making and styling the header

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,3 @@
+export { default as RefreshIcon } from "./refresh.png";
+export { default as SelectIcon } from "./select.png";
+export { default as SortIcon } from "./sort.png";
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import "./header.scss";
+import { Logo } from "../../assets";
+import { RefreshIcon } from "../../assets/icon";
+
 const Header: React.FC<{}> = () => {
-    return <h1>I'm the header</h1>;
+  return (
+    <header className="header">
+      <div className="header__title">
+        <img className="header__title-logo" src={Logo} alt="Space X logo" />
+        <h1 className="header__title-text">Launches</h1>
+      </div>
+      <button className="header__button" type="button">
+        Reload Data
+        <img
+          className="header__button-icon"
+          src={RefreshIcon}
+          alt="Refresh Icon"
+        />
+      </button>
+    </header>
+  );
 };
 
 export default Header;
